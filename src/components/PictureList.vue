ec5f0bf437e3eae9fafa88a1d9c5d4c3b5d884a3
jige1024 <2576366072@qq.com>
2025-03-09 23:03:33 +0800

    鸡哥智能协同云图库项目 ----图片功能扩展 ---图片分享功能

--- src/components/PictureList.vue
+++ src/components/PictureList.vue
@@ -1,64 +1,64 @@
 <template>
-  <a-list
-    :data-source="dataList"
-    :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
-    :loading="loading"
-  >
-    <template #renderItem="{ item: picture }">
-      <a-list-item style="padding: 0">
-        <!-- 单张图片 -->
-        <a-card hoverable @click="doClickPicture(picture)">
-          <template #cover>
-            <img
-              :alt="picture.name"
-              :src="picture.thumbnailUrl ?? picture.url"
-              loading="lazy"
-              style="height: 180px; object-fit: cover"
-            />
-          </template>
-          <a-card-meta :title="picture.name">
-            <template #description>
-              <a-flex>
-                <a-tag color="green">
-                  {{ picture.category ?? '默认' }}
-                </a-tag>
-                <a-tag v-for="tag in picture.tags" :key="tag" style="color: dodgerblue">
-                  {{ tag }}
-                </a-tag>
-              </a-flex>
+  <div class="picture-list">
+    <!-- 图片列表 -->
+    <a-list
+      :data-source="dataList"
+      :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
+      :loading="loading"
+    >
+      <template #renderItem="{ item: picture }">
+        <a-list-item style="padding: 0">
+          <!-- 单张图片 -->
+          <a-card hoverable @click="doClickPicture(picture)">
+            <template #cover>
+              <img
+                :alt="picture.name"
+                :src="picture.thumbnailUrl ?? picture.url"
+                style="height: 180px; object-fit: cover"
+              />
             </template>
-          </a-card-meta>
-          <template v-if="showOp" #actions>
-            <a-space @click="(e) => doSearch(picture, e)">
-              <SearchOutlined />
-              搜索
-            </a-space>
-            <a-space @click="(e) => doEdit(picture, e)">
-              <EditOutlined />
-              编辑
-            </a-space>
-            <a-space @click="(e) => doDelete(picture, e)">
-              <DeleteOutlined />
-              删除
-            </a-space>
-          </template>
-        </a-card>
-      </a-list-item>
-    </template>
-  </a-list>
+            <a-card-meta :title="picture.name">
+              <template #description>
+                <a-flex>
+                  <a-tag color="green">
+                    {{ picture.category ?? '默认' }}
+                  </a-tag>
+                  <a-tag v-for="tag in picture.tags" :key="tag">
+                    {{ tag }}
+                  </a-tag>
+                </a-flex>
+              </template>
+            </a-card-meta>
+            <ShareModal ref="shareModalRef" :link="shareLink" />
+            <template v-if="showOp" #actions>
+              <search-outlined @click="(e) => doSearch(picture, e)" />
+              <share-alt-outlined @click="(e) => doShare(picture, e)" />
+              <edit-outlined @click="(e) => doEdit(picture, e)" />
+              <delete-outlined @click="(e) => doDelete(picture, e)" />
+            </template>
+          </a-card>
+        </a-list-item>
+      </template>
+    </a-list>
+  </div>
 </template>
 
 <script lang="ts" setup>
-
-// 定义数据
-import { SearchOutlined, EditOutlined,DeleteOutlined } from '@ant-design/icons-vue'
 import { useRouter } from 'vue-router'
+import {
+  DeleteOutlined,
+  EditOutlined,
+  SearchOutlined,
+  ShareAltOutlined,
+} from '@ant-design/icons-vue'
 import { deletePictureUsingPost } from '@/api/pictureController.ts'
 import { message } from 'ant-design-vue'
+import { ref } from 'vue'
+import ShareModal from "@/components/ShareModal.vue";
 
 interface Props {
-  dataList?: API.PictureVO[],
-  loading?: boolean,
+  dataList?: API.PictureVO[]
+  loading?: boolean
   showOp?: boolean
   onReload?: () => void
 }
@@ -66,19 +66,24 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   dataList: () => [],
   loading: false,
-  showOp: false
+  showOp: false,
 })
 
 const router = useRouter()
 // 跳转至图片详情页
 const doClickPicture = (picture: API.PictureVO) => {
   router.push({
-    path: `/picture/${picture.id}`
+    path: `/picture/${picture.id}`,
   })
 }
 
+// 搜索
+const doSearch = (picture, e) => {
+  e.stopPropagation()
+  window.open(`/search_picture?pictureId=${picture.id}`)
+}
+
 // 编辑
-// 需要用 e.stopPropagation 阻止事件传播，否则会同时触发卡片点击事件，跳转到图片详情页
 const doEdit = (picture, e) => {
   // 阻止冒泡
   e.stopPropagation()
@@ -87,13 +92,12 @@ const doEdit = (picture, e) => {
     path: '/add_picture',
     query: {
       id: picture.id,
-      spaceId: picture.spaceId
-    }
+      spaceId: picture.spaceId,
+    },
   })
 }
 
 // 删除数据
-// 需要用 e.stopPropagation 阻止事件传播，否则会同时触发卡片点击事件，跳转到图片详情页
 const doDelete = async (picture, e) => {
   // 阻止冒泡
   e.stopPropagation()
@@ -110,15 +114,20 @@ const doDelete = async (picture, e) => {
   }
 }
 
-// 搜索
-const doSearch = (picture, e) => {
+// 分享弹窗引用
+const shareModalRef = ref()
+// 分享链接
+const shareLink = ref<string>()
+
+// 分享
+const doShare = (picture: API.PictureVO, e: Event) => {
   e.stopPropagation()
-  window.open(`/search_picture?pictureId=${picture.id}`)
+  shareLink.value = `${window.location.protocol}//${window.location.host}/picture/${picture.id}`
+  if (shareModalRef.value) {
+    shareModalRef.value.openModal()
+  }
 }
-
-
 </script>
 
 <style scoped></style>
-
 
